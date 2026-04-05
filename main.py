5dceba83ac956febc8015089c49e4d0aaa54f431
abderrahim <abderahimbensaid111@gmail.com>
2026-04-05 19:09:41 +0100

    add pre-trained model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import cv2 as cv
 import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf 
@@ -18,7 +19,74 @@ for name ,path in tqdm(zip(image_names,image_paths)) :
     image_dataframe.iloc[i]["image_name"] = name 
     image_dataframe.iloc[i]["path"] = path
     i = i + 1
-print(image_dataframe.head())
+simple_images = []
+def get_images() : 
+    sample_images = []
+    random_image_paths = [np.random.choice(image_dataframe["path"])for i in range(6)]
+    plt.figure(figsize=(12,8))
+    for i in range(6) : 
+        plt.subplot(2,3,i+1)
+        image= cv.imread(random_image_paths[i])
+        image = cv.cvtColor(image , cv.COLOR_BGR2RGB)
+        sample_images.append(image)
+        plt.imshow(image,cmap= "gray")
+        plt.grid(False)
+    plt.tight_layout() 
+    plt.show()
+    return sample_images
+simple_images = get_images() 
+
+def load_my_image() : 
+    my_image_01 = cv.imread("face1.jpg")
+    my_image_01 = cv.cvtColor(my_image_01, cv.COLOR_BGR2RGB)
+
+    my_image_02 = cv.imread("face1.jpg")
+    my_image_02 = cv.cvtColor(my_image_02, cv.COLOR_BGR2RGB)
+    return my_image_01,my_image_02
+
+img1,img2 = load_my_image()
+
+def haar_cascade_detection(simple_images):
+    face_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    for image in tqdm(simple_images) : 
+        gray = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.05, 5, 50)
+        
+        for (x_coordinate, y_coordinate, height, width) in faces : 
+            cv.rectangle(image, (x_coordinate, y_coordinate), (x_coordinate + width, y_coordinate + height), (100, 0, 0), 2)
+
+haar_cascade_detection([img1,img2])
+# plt.figure(figsize = (12, 8))
+# for i in range(6) : 
+#     plt.subplot(2,3, i+1)
+#     plt.imshow(simple_images[i], cmap = "gray")
+#     plt.title("Image {}".format(i+1))
+#     plt.grid(False)
+# plt.tight_layout()
+
+plt.figure(figsize = (12, 8))
+plt.subplot(1,2,1) 
+plt.title("The One During A Conference", fontsize = 13)
+plt.imshow(img1, cmap = "gray")
+plt.grid(False)
+
+plt.subplot(1,2,2) 
+plt.title("The One With Home Buddies!", fontsize = 13)
+plt.imshow(img2, cmap = "gray")
+plt.grid(False)
+
+plt.tight_layout()
+plt.show()
+
+
+
+
+
+
+ 
+
+
+
 
 
 
